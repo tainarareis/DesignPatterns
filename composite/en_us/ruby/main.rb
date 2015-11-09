ddfe8039b9c733c109f11dc9eb47e47782997c99
Arkye <arkye@hotmail.com.br>
2015-11-09 09:19:39 -0500

    Update the use of Agile Requirements

--- composite/en_us/ruby/main.rb
+++ composite/en_us/ruby/main.rb
@@ -6,14 +6,15 @@ require './composite_requirement'
 dash_board = CompositeRequirement.new
 
 # create and only set the title
-user_story = UserStory.new("Apply Composite", nil)
-user_story.format('programmer', 'apply this pattern', 'work better')
+user_story = UserStory.new("Apply Composite in Ruby Language", nil)
+user_story.format("student", "use a Composite example pattern in Ruby Language",
+	"I can understand how to use the pattern")
 dash_board.add(user_story)
 
-epic = Epic.new("Increase quality", "Increase code quality of project")
-dash_board.add(epic)
-
-feature = Feature.new("Refactor code", 'Apply good techniques')
+feature = Feature.new("Structural Patterns", "GOF Structural Examples")
 dash_board.add(feature)
+
+epic = Epic.new("GOF Patterns", "Entire GOF Patterns exemplified")
+dash_board.add(epic)
 
 dash_board.show
